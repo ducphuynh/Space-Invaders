49c48ec1c0286242280a03a54c67ef20be95e0ed
ducphuynh <31796456+ducphuynh@users.noreply.github.com>
2019-10-14 06:29:48 -0700

    Project 3 Space Invaders

--- alien.py
+++ alien.py
@@ -3,7 +3,6 @@ from pygame.sprite import Sprite
 
 class Alien(Sprite):
     """A class to represent a single alien in the fleet."""
-
     def __init__(self, ai_settings, screen):
         """Initialize the alien, and set its starting position."""
         super(Alien, self).__init__()
--- game_functions.py
+++ game_functions.py
@@ -26,8 +26,7 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ai_settings, screen, stats, sb, play_button, ship, aliens,
-        bullets):
+def check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
